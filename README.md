ae1a8bb893d8f87f8da9672eeb8c4a48945ed9f1
ngxson <thichthat@gmail.com>
2018-06-08 16:52:30 +0700

    init

---
+++ README.md
@@ -0,0 +1,103 @@
+### Mapping PSID to FBID
+
+This package allows you to match page-specific id (psid) with user's real id (id starts with 1000...) from webhook events.
+**Requirement:** You must have **admin** or **editor** role on that page.
+
+---------------------------------------
+### Getting started
+
+This package uses **Android** or **iOS access_token**. To get it, follow these instructions:
+* Login to your facebook account on a browser (on PC)
+* Go to your profile page (https://facebook.com/profile.php)
+* View HTML source code (Ctrl+U in Chrome)
+* Press Ctrl+F to find on that page
+* Type in the search box `EAAA`
+* Get the access_token which starts with `EAAA`...
+
+---------------------------------------
+### How to use
+
+For example you have a page with id = `182794865548469`, and your Android token is `EAAAAUaZA8jlABAIv...`
+Firstly, you must setup the package like this:
+```js
+const FbidToPsid = require('fbid-to-psid');
+FbidToPsid.init("182794865548469",
+        "EAAAAUaZA8jlABAIv...",
+        {cache_enable: true})
+.then(() => {
+    console.log("Setup complete");
+}).catch(() => {
+    console.log("Setup failed");
+})
+```
+
+After that, you can use these functions:
+* [`getFromWebhookEvent(messaging_event)`](#getFromWebhookEvent)
+* [`getFromMid(mid, psid)`](#getFromMid)
+
+---------------------------------------
+<a name="getFromWebhookEvent"></a>
+### getFromWebhookEvent(messaging_event)
+
+Get fbid from messaging_event of webhook
+This **does NOT work** with postback from buttons / Get Started button
+
+__Arguments__
+
+* `@param {Object} messaging_event` messaging_event from webhook.
+* `@returns {Promise}` Returns Promise resolve(fbid), with fbid = null if there's an error.
+
+__Example__
+
+```js
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging;
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i];
+		let psid = event.sender.id;
+		FbidToPsid.getFromWebhookEvent(event).then(fbid => {
+		    console.log("Got psid = "+psid+", fbid = "+fbid);
+		})
+	}
+	res.sendStatus(200);
+})
+```
+
+---------------------------------------
+<a name="getFromMid"></a>
+### getFromMid(mid, psid)
+
+Get fbid from mid (message_id)
+For example, each time you send a message via `/me/messages` endpoint, you receive an object which contains `message_id`. The `message_id` can then be passed into getFromMid to get fbid of the receiver.
+
+__Arguments__
+
+ * `@param {String} mid` message_id.
+ * `@param {String} psid` Optional, user's psid. Used for cache feature.
+ * `@returns {Promise}` Returns Promise resolve(fbid), with fbid = null if there's an error.
+
+__Example__
+
+```js
+request({
+	url: 'https://graph.facebook.com/v2.6/me/messages',
+	qs: {access_token:token},
+	method: 'POST',
+	json: {
+		recipient: {id:psid},
+		message: messageData,
+	}
+}, function(error, response, body) {
+	if (!error && !response.body.error && response.body.message_id) {
+	    var mid = response.body.message_id;
+		FbidToPsid.getFromMid(mid, psid).then(fbid => {
+		    console.log("Got psid = "+psid+", fbid = "+fbid);
+		})
+	}
+})
+```
+
+---------------------------------------
+### Author
+* ngxson (Nui Nguyen)
+* My website: https://ngxson.com
---
+++ index.js
@@ -0,0 +1,151 @@
+const _ = require('lodash');
+const request = require('request');
+const state = {
+    page_id: null,
+    page_token: null,
+    cache_enable: true,
+    cache: {}
+}
+const ERR_NO_PAGE_TOKEN = "Cannot find page_token, make sure that you've use init(page_id, access_token, options)";
+
+/**
+ * Setup function
+ *
+ * @param {String} page_id The array to iterate over.
+ * @param {String} access_token Android or iOS access_token.
+ * @returns {Promise} Returns Promise resolve() if success.
+ */
+function init(page_id, access_token, options) {
+    state.page_id = page_id;
+    if (options)
+        _.merge(state, options);
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://graph.facebook.com/me/accounts',
+            qs: {
+                access_token: access_token
+            },
+            method: 'GET'
+        }, function(error, response, body) {
+            if (error) {
+                console.error(error);
+                reject();
+            } else {
+                var data = JSON.parse(body);
+                data.data.forEach(page => {
+                    if (page.id == page_id) {
+                        state.page_token = page.access_token;
+                    }
+                });
+                if (state.page_token) {
+                    resolve();
+                } else {
+                    console.error("Cannot find page. Please check your page_id and access_token");
+                    console.error("access_token must be taken from source code of fb.com/profile.php (Ctrl+U in Chrome to view source code, then Ctrl+F to find token starts with EAAA)");
+                    reject();
+                }
+            }
+        })
+    })
+}
+
+/**
+ * Get fbid from messaging_event of webhook
+ * This does NOT work with postback from buttons / Get Started button
+ *
+ * @param {Object} messaging_event messaging_event from webhook.
+ * @returns {Promise} Returns Promise resolve(fbid), with fbid = null if there's an error.
+ */
+function getFromWebhookEvent(messaging_event) {
+    return new Promise((resolve, reject) => {
+        if (!messaging_event.sender) {
+            console.error("Invalid messaging_event object");
+            return reject();
+        } else if (!state.page_token) {
+            console.error(ERR_NO_PAGE_TOKEN);
+            return resolve(null);
+        } else if (!messaging_event.message || !messaging_event.message.mid) {
+            return resolve(null);
+        }
+        var mid = messaging_event.message.mid,
+            psid = messaging_event.sender.id;
+
+        if (state.cache_enable && state.cache[psid]) {
+            return resolve(state.cache[psid]);
+        }
+
+        getFromMid(mid, psid).then((fbid) => {
+            resolve(fbid);
+        }).catch(() => {
+            reject();
+        })
+    })
+}
+
+/**
+ * Get fbid from mid (message_id)
+ *
+ * @param {String} mid message_id.
+ * @param {String} psid Optional, user's psid.
+ * @returns {Promise} Returns Promise resolve(fbid), with fbid = null if there's an error.
+ */
+function getFromMid(mid, psid) {
+    return new Promise((resolve, reject) => {
+        if (!state.page_token) {
+            console.error(ERR_NO_PAGE_TOKEN);
+            return resolve(null);
+        } else if (!mid) {
+            return resolve(null);
+        }
+
+        if (psid && state.cache_enable && state.cache[psid]) {
+            return resolve(state.cache[psid]);
+        }
+
+        if (!mid.startsWith('m_')) {
+            mid = 'm_'+mid;
+        }
+
+        request({
+            url: 'https://graph.facebook.com/'+mid,
+            qs: {
+                access_token: state.page_token,
+                fields: "from,to"
+            },
+            method: 'GET'
+        }, function(error, response, body) {
+            if (error) {
+                console.error(error);
+                resolve(null);
+            } else {
+                var res = JSON.parse(body);
+                if (res.error) {
+					console.error("getFromMid "+mid+" ERR: "+res);
+                    resolve(null);
+                    
+				} else if (res["from"].id != state.page_id) {
+                    if (state.cache_enable) {
+                        state.cache[psid] = res["from"].id;
+                    }
+                    resolve(res["from"].id);
+
+				} else if (res.to.data[0].id != state.page_id) {
+                    if (state.cache_enable) {
+                        state.cache[psid] = res.to.data[0].id;
+                    }
+                    resolve(res.to.data[0].id);
+
+				} else {
+					console.error("getFromMid "+mid+" ERR: "+res);
+					resolve(null);
+				}
+            }
+        })
+    })
+}
+
+module.exports = {
+    init: init,
+    getFromMid: getFromMid,
+    getFromWebhookEvent: getFromWebhookEvent
+}
